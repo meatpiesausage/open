ea0ee6e6f116a6d6a08b01ad2014557da0e7f194
mark <taylormark82@gmail.com>
2025-05-26 11:17:15 +0100

    lots of magical files

---
+++ bin/app.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { NodejsFargateStack } from '../lib/nodejs-fargate-stack';
+
+const app = new cdk.App();
+
+// Development environment stack
+new NodejsFargateStack(app, 'NodejsFargateDevStack', {
+  appName: 'nodejs-fargate-app',
+  environment: 'dev',
+  containerPort: 3000,
+  desiredCount: 2,
+  cpu: 256,
+  memory: 512,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'eu-west-2',
+  },
+});
+
+// Production environment stack (optional)
+new NodejsFargateStack(app, 'NodejsFargateProdStack', {
+  appName: 'nodejs-fargate-app',
+  environment: 'prod',
+  containerPort: 3000,
+  desiredCount: 4,
+  cpu: 512,
+  memory: 1024,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'eu-west-2',
+  },
+});
